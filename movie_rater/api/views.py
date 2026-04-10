31e0a29dfe19b903d99d3fca57009f08f7751966
Emanuelly Karine <emanuellyfernandes77@gmail.com>
2026-04-10 18:15:51 -0300

    feat: :sparkles: Permissions and authtoken
    Creating the token to authentication and putting permissions in the classes

--- movie_rater/api/views.py
+++ movie_rater/api/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from .models import Movie, Rating
-from .serializers import MovieSerializer, RatingSerializer
+from .serializers import MovieSerializer, RatingSerializer, UserSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 # Create your views here.
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
 
     @action(detail=True, methods=['POST']) #detail true informa que a função é a respeito de um objeto específico e vc precisa passar pk 
     def rate_movie(self, request, pk=None):
@@ -38,6 +46,13 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
 
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'You cant update rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
-
+    def create(self, request, *args, **kwargs):
+        response = {'message': 'You cant create rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
